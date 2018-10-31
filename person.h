1e9f5726df5a69396146c8c1db60d5f2b3017797
Caleb Parikh <parikhcaleb@gmail.com>
2018-10-31 15:30:31 +0000

    Add files via upload

---
+++ person.h
@@ -0,0 +1,23 @@
+#ifndef PERSON_H_
+#define PERSON_H_
+
+private:
+//variables informing internal logic of agent
+	int age;
+	bool gender;
+	char mode;
+	int affluence;
+
+public:
+
+//get methods
+	void getAge();
+	void getGender);
+	void getMode();
+	void getAffluence();
+
+//set methods
+	void setMode(); //may not be neccesarry
+
+
+#endif /* HUMAN_H_ */
